9333f157e85457efe0f2b483e60273f0ddcfa38f
Alex Stopar <stopar.2@osu.edu>
2015-07-08 14:52:00 -0400

    Added TextWindow object to separate words from
    TextMaster object.

--- Assets/Resources/Scripts/TextMaster.cs
+++ Assets/Resources/Scripts/TextMaster.cs
@@ -8,11 +8,10 @@ public class TextMaster : MonoBehaviour {
 	List<GameObject> createdWords;
 	List<string> possibleWords;
 	public GameObject targetedWord;
-	Vector3 originalWordScale; //Scale for original word when pulsing
+
 	public Font typeFont;
 	public int fontSize = 20;
-	public float pulseSpeed = 0.1f;
-	PulseType currentPulse = PulseType.NONE;
+	public float pulseSpeed = 0.03f;
 	// Use this for initialization
 	void Start () {
 		createdWords = new List<GameObject> ();
@@ -23,60 +22,30 @@ public class TextMaster : MonoBehaviour {
 
 		createdWords.Add(CreateMesh(possibleWords[0]));
 		targetedWord = createdWords[0];
-		originalWordScale = targetedWord.transform.localScale;
 	}
 
 	void LoadWords() {
-		possibleWords.Add ("Example");
+		possibleWords.Add ("Tell it like it is");
 	}
 
 	GameObject CreateMesh(string targetWord)
 	{
 		GameObject textMesh = new GameObject (targetWord);
-		textMesh.AddComponent<TextMesh>();
-		MeshRenderer meshRender = textMesh.GetComponent<MeshRenderer>();
-		meshRender.material = Resources.Load<Material>("Fonts/unispace");
-		TextMesh mesh = textMesh.GetComponent<TextMesh>();
-		mesh.text = targetWord;
-		mesh.font = typeFont;
-		mesh.fontSize = fontSize;
-		mesh.color = Color.green;
+		textMesh.AddComponent<TypeWindow> ();
+		textMesh.GetComponent<TypeWindow>().CreateWord (targetWord, fontSize, typeFont);
 		textMesh.transform.parent = transform;
-		mesh.anchor = TextAnchor.MiddleCenter;
 		return textMesh;
+		
 	}
-	void CutMesh()
-	{
-		if (targetedWord.name.Length > 0) {
-			targetedWord.name = targetedWord.name.Substring (1);
-			targetedWord.GetComponent<TextMesh> ().text = 
-				targetedWord.GetComponent<TextMesh> ().text.Substring (1);
-		}
-		if (targetedWord.name.Equals (""))
-			GameObject.Destroy (targetedWord);
-		else
-			currentPulse = PulseType.IN;
-	}
+
 	// Update is called once per frame
 	void Update () {
 		if (targetedWord != null && Input.inputString.Length > 0 && 
 		    Input.inputString.Contains(targetedWord.name[0].ToString()))
-			CutMesh ();
+			targetedWord.GetComponent<TypeWindow>().CutMesh ();
 
-		if(targetedWord != null) PulseMesh ();
+		if(targetedWord != null) targetedWord.GetComponent<TypeWindow>().PulseMesh (pulseSpeed);
 	}
 
-	void PulseMesh()
-	{
-		if (currentPulse == PulseType.NONE)
-			return;
-		else if (currentPulse == PulseType.IN) targetedWord.transform.localScale -= new Vector3 (pulseSpeed, pulseSpeed, pulseSpeed);
-		else targetedWord.transform.localScale += new Vector3 (pulseSpeed, pulseSpeed, pulseSpeed);
 
-		if (targetedWord.transform.localScale.x >= originalWordScale.x)
-			currentPulse = PulseType.NONE;
-		else if (targetedWord.transform.localScale.x <= originalWordScale.x / 2.0f)
-			currentPulse = PulseType.OUT;
-
-	}
 }
---
+++ Assets/Resources/Scripts/TypeWindow.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TypeWindow : MonoBehaviour {
+
+	GameObject typeTarget; //Where words take place
+	GameObject typeBackground; //Background that sizes with word
+	Vector3 originalWordScale; //Scale for original word when pulsing
+	float wordHeight = 1.0f;
+	PulseType currentPulse = PulseType.NONE;
+	// Use this for initialization
+	void Start () {
+		typeBackground = new GameObject ();
+
+	}
+
+	public void CreateWord(string targetWord, int fontSize, Font typeFont)
+	{
+		typeTarget = new GameObject ("textWords");
+		typeTarget.AddComponent<TextMesh>();
+		MeshRenderer meshRender = typeTarget.GetComponent<MeshRenderer>();
+		meshRender.material = Resources.Load<Material>("Fonts/unispace");
+		TextMesh mesh = typeTarget.GetComponent<TextMesh>();
+		mesh.text = targetWord;
+		mesh.font = typeFont;
+		mesh.fontSize = fontSize;
+		mesh.color = Color.green;
+		typeTarget.transform.parent = transform;
+		mesh.anchor = TextAnchor.MiddleCenter;
+		if (meshRender.bounds.size.y > wordHeight) {
+			this.transform.localScale *= wordHeight/meshRender.bounds.size.y;
+		}	
+		originalWordScale = this.transform.localScale;
+	}
+
+	public void CutMesh()
+	{
+		if (this.name.Length > 0) {
+			this.name = this.name.Substring (1);
+			typeTarget.GetComponent<TextMesh> ().text = 
+				typeTarget.GetComponent<TextMesh> ().text.Substring (1);
+		}
+		if (this.name.Equals (""))
+			GameObject.Destroy (typeTarget);
+		else
+			currentPulse = PulseType.IN;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PulseMesh(float pulseSpeed)
+	{
+		if (currentPulse == PulseType.NONE)
+			return;
+		else if (currentPulse == PulseType.IN) this.transform.localScale -= new Vector3 (pulseSpeed, pulseSpeed, pulseSpeed);
+		else this.transform.localScale += new Vector3 (pulseSpeed, pulseSpeed, pulseSpeed);
+		
+		if (this.transform.localScale.x >= originalWordScale.x)
+			currentPulse = PulseType.NONE;
+		else if (this.transform.localScale.x <= originalWordScale.x / 2.0f)
+			currentPulse = PulseType.OUT;
+		
+	}
+}
